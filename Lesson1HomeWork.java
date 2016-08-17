44eef914b0ee594a625f24fa0df3c8ee26ac77f2
usonictw <usonictw@bigmir.net>
2016-08-17 22:49:11 +0300

    lesson1 home work

---
+++ Lesson1HomeWork.java
@@ -0,0 +1,10 @@
+
+public class Lesson1HomeWork{
+
+	public static void main(String[] args) {
+		System.out.println("Mishchenko Volodymyr");
+		System.out.println("Liskovskaya 12 str.");
+		System.out.println("Kyiv");
+		System.out.println("Ukraine");
+	}
+}
